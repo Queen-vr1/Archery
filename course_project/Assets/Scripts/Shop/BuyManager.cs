4fcece37df355494f251bf26934c10c0beea2474
Patricia <825907@unizar.es>
2025-04-25 20:04:57 +0200

    confirmacion compra

---
+++ course_project/Assets/Scripts/Shop/BuyManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.UI;
+
+public class BuyManager : MonoBehaviour
+{
+	[System.Serializable]
+	public class Item
+	{
+		public XRBaseInteractable interactable;
+		public GameObject buyPanel;
+		public Button yesButton;
+		public Button noButton;
+	}
+
+	public List<Item> items = new List<Item>();
+
+	public PanelManager panelManager;
+
+	void Start()
+	{
+		Debug.Log("Start");
+		foreach (var item in items)
+		{
+			item.interactable.selectEntered.AddListener((args) => OnItemSelected(item));
+
+			item.yesButton.onClick.AddListener(() => OnYesButtonClicked(item));
+			item.noButton.onClick.AddListener(() => OnNoButtonClicked(item));
+
+			item.buyPanel.SetActive(false);
+		}
+	}
+
+	void OnItemSelected(Item item)
+	{
+		Debug.Log("Seleccionado");
+		foreach (var pair in panelManager.pairs)
+		{
+			pair.panel.SetActive(false);
+		}
+
+		item.buyPanel.SetActive(true);
+	}
+
+	void OnYesButtonClicked(Item item)
+	{
+		Debug.Log("Holi");
+		item.buyPanel.SetActive(false);
+	}
+
+	void OnNoButtonClicked(Item item)
+	{
+		Debug.Log("Bye");
+		item.buyPanel.SetActive(false);
+	}
+}
