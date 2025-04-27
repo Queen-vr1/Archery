f6edef5493473163bb02d0d84e1c573b49de8470
Patricia <825907@unizar.es>
2025-04-27 14:43:26 +0200

    ui

--- course_project/Assets/Scripts/Manager/GameMenuManager.cs
+++ course_project/Assets/Scripts/Manager/GameMenuManager.cs
@@ -7,7 +7,9 @@ public class GameMenuManager : MonoBehaviour
 {
     public Transform head;
     public float spawnDistance = 3f;
+    public float spawnHeight = 1.5f;
     public GameObject menu;
+
     void Start()
     {
         menu.SetActive(true);
@@ -22,6 +24,7 @@ public class GameMenuManager : MonoBehaviour
     void LookAtPlayer()
     {
         menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+        menu.transform.position = new Vector3(menu.transform.position.x, head.position.y + spawnHeight, menu.transform.position.z);
         menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
         menu.transform.forward *= -1;
     }
